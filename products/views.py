9ea40a50e2c85baedff25eabcb6e0aaa7430c191
mrbrunotte <mrbrunotte@gmail.com>
2020-03-02 00:37:11 +0100

    Add cart

--- products/views.py
+++ products/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
-"""
-So the first view we're going to create is all_products.
-In this case, we take products.
-And that's going to be product.objects.all, which will return all the products that are in the database.
-And we're going to render a products.html page, which we have yet to write.
-And within that page, we will have access to all products.
-"""
-
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
